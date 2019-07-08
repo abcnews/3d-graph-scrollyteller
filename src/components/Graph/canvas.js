2f9d453d073149296ee57c3968e99d2e011b9714
Joshua Byrd <phocks@gmail.com>
2019-07-08 15:23:26 +1000

    IE11 calc in transform fix

--- src/components/Graph/canvas.js
+++ src/components/Graph/canvas.js
@@ -283,6 +283,11 @@ export default class Canvas {
         if (n.isVisible) {
           const screenPosition = worldToScreen(n.obj.position, this.camera);
           n.labelElement.style.setProperty(
+            "-ms-transform",
+            `translateX(${screenPosition.x}px) translateX(-50%)
+            translateY(${screenPosition.y + 25 + (1 / screenPosition.z) * 50}px)`
+          );
+          n.labelElement.style.setProperty(
             "transform",
             `translate(calc(${screenPosition.x}px - 50%), ${screenPosition.y +
               25 + (1 / screenPosition.z) * 50}px)`
